a2b947fe2392ce6bb17bf68325b2e58ba3c53e32
Andrej Bauer <Andrej.Bauer@andrej.com>
2011-01-09 18:05:42 +0100

    added explanation for graph_via_action.py
    --HG--
    rename : theories/symmetric_graph.th => theories/graph_via_action.py

--- generate.ml
+++ generate.ml
@@ -42,6 +42,75 @@ let generate n ({T.th_const=const; T.th_equations=eqs; T.th_axioms=axs} as th) k
     let pred = a.alg_predicates in
     let rel = a.alg_relations in
 
+    (* Partial evlauation of a term. It returns the partially evaluated term together
+       with a count of how many table entries need to be filled in for the term to 
+       become completely evaluated. *)
+    let rec eval_term = function
+      | T.Var i -> Error.fatal "eval_term: variable encountered"
+      | T.Elem e -> (T.Elem e, e, 0)
+      | T.Const i -> (T.Elem const.(i), const.(i), 0) (* NB: We assume constants are always defined. *)
+      | T.Unary (op, t) ->
+          let (t, v, k) = eval_term t in
+            if v = -1 || unary.(op).(v) = -1 then (T.Unary (op, t), -1, k+1)
+            else (T.Elem unary.(op).(v), unary.(op).(v), 0)
+      | T.Binary (op, t1, t2) ->
+          let (t1, v1, k1) = eval_term t1 in
+          let (t2, v2, k2) = eval_term t2 in
+            if v1 = -1 || v2 = -1 || binary.(op).(v1).(v2) = -1 then (T.Binary (op, t1, t2), -1, k1+k2+1)
+            else let u = binary.(op).(v1).(v2) in (T.Elem u, u, 0)
+    in
+
+    let rec eval_formula = function
+      | T.True -> (T.True, 1, 0)
+      | T.False -> (T.False, 0, 0)
+      | T.Predicate (p, t) ->
+          let (t, v, k) = eval_term t in
+            if v = -1 || pred.(p).(v) = -1 then (T.Predicate (p, t), -1, k+1)
+            else if pred.(p).(v) = 1 then (True, 1, 0) else (False, 0, 0)
+      | T.Relation (r, t1, t2) ->
+          let (t1, v1, k1) = eval_term t1 in
+          let (t2, v2, k2) = eval_term t2 in
+            if v1 = -1 || v2 = -1 || rel.(r).(v1).(v2) = -1 then (T.Relation (r, t1, t2), -1, k1+k2+1)
+            else if rel.(r).(v1).(v2) = 1 then (True, 1, 0) else (False, 0, 0)
+      | T.Equal (t1, t2) ->
+          let (t1, v1, k1) = eval_term t1 in
+          let (t2, v2, k2) = eval_term t2 in
+            if v1 = -1 || v2 = -1 then (T.Equal (t1, t2), -1, k1 + k2)
+            else if v1 = v2 then (T.True, 1, 0)
+            else (T.False, 0, 0)
+      | T.Not f ->
+          let (f, v, k) = eval_formula f in
+            if v = -1 then (T.Not f, -1, k)
+            else if v = 1 then (T.False, 0, 0)
+            else (T.True, 1, 0)
+      | T.And (f1, f2) ->
+          let (f1, v1, k1) = eval_formula f1 in
+          let (f2, v2, k2) = eval_formula f2 in
+            if v1 = -1 || v2 = -1 then (T.And (f1, f2), -1, k1+k2)
+            else if v1 = 1 && v2 = 1 then (T.True, 1, 0)
+            else (T.False, 0, 0)
+      | T.Or (f1, f2) ->
+          let (f1, v1, k1) = eval_formula f1 in
+          let (f2, v2, k2) = eval_formula f2 in
+            if v1 = -1 || v2 = -1 then (T.Or (f1, f2), -1, k1+k2)
+            else if v1 = 1 || v2 = 1 then (T.True, 1, 0)
+            else (T.False, 0, 0)
+      | T.Imply (f1, f2) ->
+          let (f1, v1, k1) = eval_formula f1 in
+          let (f2, v2, k2) = eval_formula f2 in
+            if v1 = -1 || v2 = -1 then (T.Imply (f1, f2), -1, k1+k2)
+            else if v1 = 0 || v2 = 1 then (T.True, 1, 0)
+            else (T.False, 0, 0)
+      | T.Iff (f1, f2) ->
+          let (f1, v1, k1) = eval_formula f1 in
+          let (f2, v2, k2) = eval_formula f2 in
+            if v1 = -1 || v2 = -1 then (T.Iff (f1, f2), -1, k1+k2)
+            else if v1 = v2 then (T.True, 1, 0)
+            else (T.False, 0, 0)
+      | T.Forall _ -> Error.fatal "eval_formula: forall encountered"
+      | T.Exists _ -> Error.fatal "eval_formula: exists encountered"
+    in
+
     (* Force [t] to have value [v]. *)
     let rec force_term t v k =
       match t with
@@ -129,15 +198,20 @@ let generate n ({T.th_const=const; T.th_equations=eqs; T.th_axioms=axs} as th) k
                     end
                     else if rel.(r).(v1).(v2) = b then k ()))
         | Equal (t1, t2) ->
-            force_term t1 (-1)
-              (fun v ->
-                 if b = 1
-                 then force_term t2 v (fun _ -> k ())
-                 else begin
-                   for w = 0 to n-1 do
-                     if w <> v then force_term t2 w (fun _ -> k ())
-                   done
-                 end)
+            let (t1, v1, k1) = eval_term t1 in
+            let (t2, v2, k2) = eval_term t2 in
+              if v1 <> -1 && v2 <> -1 then (if v1 = v2 then k ())
+              else
+                let (s1, s2) = if k1 < k2 then (t1,t2) else (t2,t1) in
+                  force_term s1 (-1)
+                    (fun v ->
+                       if b = 1
+                       then force_term s2 v (fun _ -> k ())
+                       else begin
+                         for w = 0 to n-1 do
+                           if w <> v then force_term s2 w (fun _ -> k ())
+                         done
+                       end)
         | Not f -> force_formula f (1-b) k
         | And (f1, f2) ->
             if b = 1 then
@@ -169,10 +243,8 @@ let generate n ({T.th_const=const; T.th_equations=eqs; T.th_axioms=axs} as th) k
               force_formula f1 0 (fun () -> force_formula f2 0 k) ;
               force_formula f1 1 (fun () -> force_formula f2 1 k)
             end
-        | Forall (i, f) ->
-            force_formula (and_of n i f) b k
-        | Exists (i, f) ->
-            force_formula (or_of n i f) b k
+        | Forall _ -> Error.fatal "force_formula: forall encountered"
+        | Exists _ -> Error.fatal "force_formula: exists encountered"
     in
       
     let force_equation (i,(t1,t2)) b k =
@@ -270,6 +342,31 @@ let generate n ({T.th_const=const; T.th_equations=eqs; T.th_axioms=axs} as th) k
         end
       in
         g 0
+    in
+
+    let rec prepare_formula acc = function
+      | Forall (i, f) ->
+          let rec loop k acc =
+            if k = n then acc
+            else 
+              prepare_formula
+                (subst_formula i (Elem k) f)
+
+              loop (k+1) (prepare_formula (subst_formula i (Elem k) f) acc)
+
+              let f = subst_formula i (Elem k) f in
+              let acc = List.fold_left (fun f acc -> ) (prepare_formula f)
+
+
+          in
+            if n = 0 then [True] else [loop 1 (subst_formula i (Elem 0) f)]
+
+
+          List.fold_left ()
+
+      
+    let prepare_equation (i, (t1, t2)) =
+      prepare_formula (List.fold_right (fun x g -> Forall (x, g)) (Util.enumFromTo 0 (i-1)) (Equal (t1, t2)))
     in
 
       force_equations eqs
